2401e6cd121cdbe240f80440c1ecb5446dc3230e
Black Swan <1124072357@qq.com>
2019-03-28 17:02:48 +0800

    Update douban_books.py

--- douban_books.py
+++ douban_books.py
@@ -1,3 +1,5 @@
+# coding=UTF-8
+
 import requests
 from requests.adapters import HTTPAdapter
 from lxml import etree
@@ -7,16 +9,14 @@ import pymysql
 import time
 import random
 import settings
+import copy
 
 # =====================================================================================================================
 base_url = 'https://book.douban.com/tag/?view=type'
 User_Agents = [
     'Mozilla/5.0(Macintosh;U;IntelMacOSX10_6_8;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50',
     'Mozilla/5.0(Windows;U;WindowsNT6.1;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50',
-    'Mozilla/5.0(Macintosh;IntelMacOSX10.6;rv:2.0.1)Gecko/20100101Firefox/4.0.1',
-    'Mozilla/5.0(WindowsNT6.1;rv:2.0.1)Gecko/20100101Firefox/4.0.1',
-    'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;360SE)',
-    'Opera/9.80(WindowsNT6.1;U;en)Presto/2.8.131Version/11.11']
+    'Mozilla/5.0(Macintosh;IntelMacOSX10.6;rv:2.0.1)Gecko/20100101Firefox/4.0.1']
 User_Agent = random.choice(User_Agents)
 headers = {
     'Host': 'book.douban.com',
@@ -36,21 +36,26 @@ class set_log(object):
     def loading_log(self, log_name):
         try:
             log_file = open(log_name)
-            log_text = list(eval(log_file.read()))
+            log_text = set(eval(log_file.read()))
+            log_file.close()
         except BaseException:
-            log_text = []
+            log_text = set()
         return log_text
 
-    def update_index_remain_type_and_book_url_list(
+
+    def update_index_remain_type_and_book_url_set(
             self,
             index_type_url,
             index_log_name,
             remain_log_name,
             book_log_name):
-        index_type_url_list.remove(index_type_url)
-        index_record_text = index_type_url_list + ['index_record']
-        remain_record_text = remain_type_url_list + ['remain_record']
-        book_record_text = book_url_list + ['book_record']
+        deep_index_type_url_set.remove(index_type_url)
+        deep_index_type_url_set.add('index_record')
+        remain_type_url_set.add('remain_record')
+        book_url_set.add('book_record')
+        index_record_text = deep_index_type_url_set
+        remain_record_text = remain_type_url_set
+        book_record_text = book_url_set
         with open(index_log_name, "w") as index_record_file:
             index_record_file.write(str(index_record_text))
         with open(remain_log_name, "w") as remain_record_file:
@@ -58,19 +63,22 @@ class set_log(object):
         with open(book_log_name, "w") as book_record_file:
             book_record_file.write(str(book_record_text))
 
-    def update_remain_type_and_book_url_list(
+    def update_remain_type_and_book_url_set(
             self, remain_type_url, remain_log_name, book_log_name):
-        remain_type_url_list.remove(remain_type_url)
-        remain_record_text = remain_type_url_list + ['remain_record']
-        book_record_text = book_url_list + ['book_record']
+        deep_remain_type_url_set.remove(remain_type_url)
+        deep_remain_type_url_set.add('remain_record')
+        book_url_set.add('book_record')
+        remain_record_text = deep_remain_type_url_set
+        book_record_text = book_url_set
         with open(remain_log_name, "w") as remain_record_file:
             remain_record_file.write(str(remain_record_text))
         with open(book_log_name, "w") as book_record_file:
             book_record_file.write(str(book_record_text))
 
-    def update_book_url_list(self, book_url, book_log_name):
-        book_url_list.remove(book_url)
-        book_record_text = book_url_list + ['book_record']
+    def update_book_url_set(self, book_url, book_log_name):
+        deep_book_url_set.remove(book_url)
+        deep_book_url_set.add('book_record')
+        book_record_text = deep_book_url_set
         with open(book_log_name, "w") as book_record_file:
             book_record_file.write(str(book_record_text))
 
@@ -93,12 +101,12 @@ class parse_data(object):
         types = base_html.xpath(
             '//*[@id="content"]//div[@class="article"]//table[@class="tagCol"]//td/a/@href')
         types_base_url = 'https://book.douban.com'
-        type_url_list = []
+        type_url_set = set()
         for type in types:
             type_url = types_base_url + type
-            type_url_list.append(type_url)
-        return type_url_list
-        time.sleep(3 + 3 * random.random())
+            type_url_set.add(type_url)
+        return type_url_set
+        time.sleep(random.randint(4, 12))
 
     def get_remain_type_urls(
             self,
@@ -140,23 +148,24 @@ class parse_data(object):
                 'comments_number': comments_number}
             update_to_mysql(type_book_data, db)
             book_url = book_sector.xpath('./div[@class="info"]/h2/a/@href')[0]
-            book_url_list.append(book_url)
+            book_url_set.add(book_url)
         db.close()
         try:
             max_page = type_html.xpath(
                 '//*[@id="subject_list"]//div[@class="paginator"]//a[last()]/text()')[0]
             for page in range(1, int(max_page)):
-                params = {'start': str(page * 20),
-                          'type': 'T'
-                          }
+                params = {
+                    'start': str(page * 20),
+                    'type': 'T'
+                }
                 remain_type_url = index_type_url + '?' + urlencode(params)
-                remain_type_url_list.append(remain_type_url)
-            log.update_index_remain_type_and_book_url_list(
+                remain_type_url_set.add(remain_type_url)
+            log.update_index_remain_type_and_book_url_set(
                 index_type_url, index_log_name, remain_log_name, book_log_name)
         except BaseException:
-            log.update_index_remain_type_and_book_url_list(
+            log.update_index_remain_type_and_book_url_set(
                 index_type_url, index_log_name, remain_log_name, book_log_name)
-        time.sleep(3 + 3 * random.random())
+        time.sleep(random.randint(4, 12))
 
     def get_book_urls(
             self,
@@ -198,11 +207,11 @@ class parse_data(object):
                 'comments_number': comments_number}
             update_to_mysql(type_book_data, db)
             book_url = book_sector.xpath('./div[@class="info"]/h2/a/@href')[0]
-            book_url_list.append(book_url)
-        log.update_remain_type_and_book_url_list(
+            book_url_set.add(book_url)
+        log.update_remain_type_and_book_url_set(
             remain_type_url, remain_log_name, book_log_name)
         db.close()
-        time.sleep(3 + 3 * random.random())
+        time.sleep(random.randint(4, 12))
 
     def get_book_data(self, book_url, book_log_name, log):
         """获取书本信息"""
@@ -219,37 +228,25 @@ class parse_data(object):
         books_informations = book_html.xpath('//*[@id="info"]//text()')
         for books_information in books_informations:
             if books_information:
-                book_data_string += books_information.strip().replace('\n', '').replace('\t',
-                                                                                        '').replace('\r', '').replace(' ', '')
+                book_data_string += books_information\
+                    .strip().replace('\n', '').replace('\t','').replace('\r', '').replace(' ', '')
         book_data = {
             'book_name': book_html.xpath('//*[@id="wrapper"]/h1/span/text()'),
             'score': book_html.xpath('//*[@id="interest_sectl"]//strong[@class="ll rating_num "]/text()')[0].strip(),
-            'author': re.findall(
-                '作者:(.*?):',
-                book_data_string)[0],
-            'publishing_house': re.findall(
-                '出版社:(.*?):',
-                book_data_string)[0],
-            'publish_year ': re.findall(
-                '出版年:(.*?):',
-                book_data_string)[0],
-            'pages_number': re.findall(
-                    '页数:(.*?):',
-                    book_data_string)[0],
-            'price': re.findall(
-                        '定价:(.*?):',
-                        book_data_string)[0],
-            'ISBN': re.findall(
-                            'ISBN:(.*)',
-                book_data_string)[0]}
+            'author': re.findall('作者:(.*?):', book_data_string)[0],
+            'publishing_house': re.findall('出版社:(.*?):', book_data_string)[0],
+            'publish_year ': re.findall('出版年:(.*?):', book_data_string)[0],
+            'pages_number': re.findall('页数:(.*?):', book_data_string)[0],
+            'price': re.findall('定价:(.*?):', book_data_string)[0],
+            'ISBN': re.findall('ISBN:(.*)', book_data_string)[0]}
         for key, value in book_data.items():
             for i in range(0, len(tags_list)):
                 if tags_list[i] in value:
                     book_data.update({key: value.strip(tags_list[i])})
         update_to_mysql(book_data, db)
-        log.update_book_url_list(book_url, book_log_name)
+        log.update_book_url_set(book_url, book_log_name)
         db.close()
-        time.sleep(3 + 3 * random.random())
+        time.sleep(random.randint(4, 12))
 
 
 def update_to_mysql(data, db):
@@ -266,30 +263,29 @@ def update_to_mysql(data, db):
     print(f'{data} update success !')
 
 
-def produce_url_list(url_list, log_list):
+def produce_url_list(url_set, log_set):
     lists = ''
-    if len(log_list):
-        x = set(url_list)
-        y = set(log_list)
-        lists = list(x & y)
+    if len(log_set):
+        lists = url_set & log_set
     else:
-        lists = url_list
+        lists = url_set
     return lists
 
 
 def run():
-    global index_type_url_list, remain_type_url_list, book_url_list
+    global index_type_url_set, remain_type_url_set, book_url_set, deep_index_type_url_set, deep_remain_type_url_set, deep_book_url_set
     log = set_log()
     info = parse_data()
-    index_type_url_list = log.loading_log('index_type_url.log')
+    index_type_url_set = log.loading_log('index_type_url.log')
     remain_type_url_log = log.loading_log('remain_type_url.log')
-    remain_type_url_list = list(set(remain_type_url_log))
+    remain_type_url_set = set(remain_type_url_log)
     book_url_log = log.loading_log('book_urls.log')
-    book_url_list = list(set(book_url_log))
-    type_url_list = info.get_index_type_url(base_url)
-    index_type_url_list = produce_url_list(type_url_list, index_type_url_list)
-    print(index_type_url_list)
-    for index_type_url in index_type_url_list:
+    book_url_set = set(book_url_log)
+    type_url_set = info.get_index_type_url(base_url)
+    index_type_url_set = produce_url_list(type_url_set, index_type_url_set)
+    print(index_type_url_set)
+    deep_index_type_url_set = copy.deepcopy(index_type_url_set)
+    for index_type_url in index_type_url_set:
         try:
             info.get_remain_type_urls(
                 index_type_url,
@@ -298,9 +294,10 @@ def run():
                 'book_urls.log',
                 log)
         except BaseException:
-            None
-    print(remain_type_url_list)
-    for remain_type_url in remain_type_url_list:
+            raise
+    print(remain_type_url_set)
+    deep_remain_type_url_set = copy.deepcopy(remain_type_url_set)
+    for remain_type_url in remain_type_url_set:
         try:
             info.get_book_urls(
                 remain_type_url,
@@ -308,13 +305,14 @@ def run():
                 'book_urls.log',
                 log)
         except BaseException:
-            None
-    print(book_url_list)
-    for book_url in book_url_list:
+            raise
+    print(book_url_set)
+    deep_book_url_set = copy.deepcopy(book_url_set)
+    for book_url in book_url_set:
         try:
             info.get_book_data(book_url, 'book_urls.log', log)
         except BaseException:
-            None
+            raise
 
 
 if __name__ == '__main__':
